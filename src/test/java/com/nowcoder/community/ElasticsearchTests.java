04e6f282c75abe80a0faa5197213860bb12c7281
xinyuKing <jin_xinyu@163.com>
2023-10-16 16:53:15 +0800

    Day13:spring整合ElasticSearch 因为ElasticSearch7进行了大更改，所以改用ElasticSearch6，也因此修改了spring-boot-starter-parent的版本。 测试了ElasticSearch的使用

---
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -0,0 +1,194 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = StudyApplication.class)
+public class ElasticsearchTests {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private DiscussPostRepository discussPostRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticTemplate;
+
+    /*插入一条数据*/
+    @Test
+    public void testInsert(){
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(244));
+
+    }
+
+    /*插入多条数据*/
+    @Test
+    public void testInsertList(){
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(101,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(102,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(103,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(111,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(112,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(131,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(132,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(133,0,100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(134,0,100));
+    }
+
+    /*修改数据*/
+    @Test
+    public void testUpdate(){
+        DiscussPost discussPost = discussPostMapper.selectDiscussPostById(231);
+        discussPost.setContent("我是新人：testUpdate");
+        discussPostRepository.save(discussPost);
+    }
+
+    /*删除数据*/
+    @Test
+    public void testDelete(){
+//        discussPostRepository.deleteById(231);
+        //删除所有数据
+        discussPostRepository.deleteAll();
+    }
+
+
+    /*搜索*/
+    @Test
+    public void testSearchByRepository(){
+        SearchQuery searchQuery=new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬","title","content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0,10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        /*//底层获取了高亮显示的值，但没有返回*/
+        Page<DiscussPost> page = discussPostRepository.search(searchQuery);
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getNumber());
+        System.out.println(page.getSize());
+        for (DiscussPost post : page) {
+            System.out.println(post);
+        }
+    }
+
+    /**/
+    @Test
+    public void testSearchByTemplate(){
+        SearchQuery searchQuery=new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬","title","content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0,10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        Page<DiscussPost> page = elasticTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse searchResponse, Class<T> aClass, Pageable pageable) {
+                //获取命中的数据
+                SearchHits hits = searchResponse.getHits();
+                //判断命中的数据是否有值
+                if(hits.getTotalHits()<=0){
+                    return null;
+                }
+
+                List<DiscussPost> list=new ArrayList<>();
+                for (SearchHit hit : hits) {
+                    DiscussPost post = new DiscussPost();
+
+                    String id = hit.getSourceAsMap().get("id").toString();
+                    post.setId(Integer.valueOf(id));
+
+                    String userId = hit.getSourceAsMap().get("userId").toString();
+                    post.setUserId(Integer.valueOf(userId));
+
+                    String title = hit.getSourceAsMap().get("title").toString();
+                    post.setTitle(title);
+
+                    String content = hit.getSourceAsMap().get("content").toString();
+                    post.setContent(content);
+
+                    String commentCount = hit.getSourceAsMap().get("commentCount").toString();
+                    post.setCommentCount(Integer.valueOf(commentCount));
+
+                    String score = hit.getSourceAsMap().get("score").toString();
+                    post.setScore(Double.valueOf(score));
+
+                    String status = hit.getSourceAsMap().get("status").toString();
+                    post.setStatus(Integer.valueOf(status));
+
+                    String type = hit.getSourceAsMap().get("type").toString();
+                    post.setType(Integer.valueOf(status));
+
+                    String createTime = hit.getSourceAsMap().get("createTime").toString();
+                    post.setCreateTime(new Date(Long.valueOf(createTime)));
+
+                    //处理高亮显示内容
+                    HighlightField titleField = hit.getHighlightFields().get("title");
+                    if (titleField!=null) {
+                        post.setTitle(titleField.getFragments()[0].toString());
+                    }
+
+                    HighlightField contentField = hit.getHighlightFields().get("content");
+                    if (contentField!=null) {
+                        post.setTitle(contentField.getFragments()[0].toString());
+                    }
+
+                    list.add(post);
+                }
+
+                return new AggregatedPageImpl(list,pageable,
+                        hits.getTotalHits(),searchResponse.getAggregations(),searchResponse.getScrollId(),hits.getMaxScore());
+            }
+        });
+
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getNumber());
+        System.out.println(page.getSize());
+        for (DiscussPost post : page) {
+            System.out.println(post);
+        }
+    }
+}
