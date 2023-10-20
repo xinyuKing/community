0db1ed358cf48a16a2bedef315ae18a2053af88a
xinyuKing <jin_xinyu@163.com>
2023-10-20 17:01:16 +0800

    Day15:生成长图 可用于分享功能

--- src/main/java/com/nowcoder/community/event/EventConsumer.java
+++ src/main/java/com/nowcoder/community/event/EventConsumer.java
@@ -12,9 +12,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +33,12 @@ public class EventConsumer implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
+
+    @Value("${wk.image.command}")
+    private String wkImageCommand;
+
+    @Value("${wk.image.storage}")
+    private String wkImageStorage;
 
     /*处理系统异步发送评论，点赞，关注的通知*/
     @KafkaListener(topics = {TOPIC_COMMENT,TOPIC_FOLLOW,TOPIC_LIKE})
@@ -69,7 +77,7 @@ public class EventConsumer implements CommunityConstant {
     }
 
     /*处理发帖时把帖子异步发送到elasticsearch中去*/
-    @KafkaListener(topics = {TOPIC_PUBLISH})
+    @KafkaListener(topics = TOPIC_PUBLISH)
     public void HandlePublish(ConsumerRecord record){
         if(record==null||record.value()==null){
             logger.error("消息内容为空！");
@@ -87,7 +95,7 @@ public class EventConsumer implements CommunityConstant {
     }
 
     /*处理删帖时在elasticsearch中异步删除该帖子*/
-    @KafkaListener(topics = {TOPIC_DELETE})
+    @KafkaListener(topics = TOPIC_DELETE)
     public void HandleDelete(ConsumerRecord record){
         if(record==null||record.value()==null){
             logger.error("消息内容为空！");
@@ -99,5 +107,30 @@ public class EventConsumer implements CommunityConstant {
             return;
         }
         elasticsearchService.deleteDiscussPost(event.getEntityId());
+    }
+
+    /*消费分享事件*/
+    @KafkaListener(topics = TOPIC_SHARE)
+    public void HandleShare(ConsumerRecord record){
+        if(record==null||record.value()==null){
+            logger.error("消息内容为空！");
+            return;
+        }
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event==null) {
+            logger.error("消息格式错误！");
+            return;
+        }
+        String htmlUrl=(String)event.getData().get("htmlUrl");
+        String fileName=(String)event.getData().get("fileName");
+        String suffix=(String)event.getData().get("suffix");
+
+        String cmd=wkImageCommand+" --quality 75 "+htmlUrl+" "+wkImageStorage+"/"+fileName+suffix;
+        try {
+            Runtime.getRuntime().exec(cmd);
+            logger.info("生成长图成功："+cmd);
+        } catch (IOException e) {
+            logger.error("生成长图失败："+e.getMessage());
+        }
     }
 }
